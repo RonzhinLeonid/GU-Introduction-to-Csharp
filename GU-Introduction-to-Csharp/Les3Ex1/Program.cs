f8f002d23a76852163a217027bfe0ebdef74f701
Leonid <jiehuh87@yandex.ru>
2021-01-23 00:26:27 +0300

    Les3 Ex1,2,3,4

---
+++ GU-Introduction-to-Csharp/Les3Ex1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3Ex1
+{
+    class Program
+    {
+        //Ронжин Л.
+        //1. Написать программу, выводящую элементы двухмерного массива по диагонали.
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Исходный двухмерный массив:");
+            int[,] arr = new int[5, 5];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rnd.Next(100);
+                    Console.Write($"{arr[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            #region Способ 1
+            Console.Write("\nСпособ №1 с простыми условиями в цикле for:");
+            Console.Write("\nГлавная диагональ: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(i==j) Console.Write($"{arr[i, j]} ");
+                }
+            }
+            Console.Write("\nПобочная диагональ: ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = arr.GetLength(1) - 1; j >= 0; j--)
+                {
+                    if (i == arr.GetLength(1) - 1 - j) Console.Write($"{arr[i, j]} ");
+                }
+            }
+            #endregion
+            #region Способ 2
+            Console.Write("\n\nСпособ №2 со сложными условиями в цикле for:");
+            Console.Write("\nГлавная диагональ: ");
+            for (int i = 0, j = 0; i < arr.GetLength(0) && j < arr.GetLength(1); i++, j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.Write("\nПобочная диагональ: ");
+            for (int i = 0, j = arr.GetLength(1) - 1; i < arr.GetLength(0) && j >=0 ; i++, j--)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            #endregion
+
+            #region Способ 2
+            Console.Write("\n\nСпособ №3 с циклом while:");
+            Console.Write("\nГлавная диагональ: ");
+            int k = 0;
+            while (k < arr.GetLength(0))
+            {
+                int z = 0;
+                while (z < arr.GetLength(1))
+                { 
+                    if (k == z) Console.Write($"{arr[k, z]} ");
+                    z++;
+                }
+                k++;
+            }
+            Console.Write("\nПобочная диагональ: ");
+            k = 0;
+            while (k < arr.GetLength(0))
+            {
+                int z = arr.GetLength(1)-1;
+                while (z >= 0)
+                {
+                    if (k == arr.GetLength(1) - 1 - z) Console.Write($"{arr[k, z]} ");
+                    z--;
+                }
+                k++;
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GU-Introduction-to-Csharp/Les3Ex4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3Ex4
+{
+    class Program
+    {
+        //Ронжин Л.
+        //4. * «Морской бой» — вывести на экран массив 10х10, состоящий из символов X и O, где Х — элементы кораблей, а О — свободные клетки.
+        static void Main(string[] args)
+        {
+            int[,] arrShip = {  { 2, 1, 4, 5, 1, 2, 9, 9, 9, 9, 7, 8, 9, 0, 0, 0, 5, 5, 5, 5},
+                                { 6, 3, 7, 9, 0, 0, 0, 1, 8, 9, 3, 3, 3, 7, 8, 9, 1, 2, 3, 4} };
+            string[,] map = new string[10, 10];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arrShip.GetLength(1); k++)
+                    {
+                        if (arrShip[0, k] == i && arrShip[1, k] == j)
+                        {
+                            map[i, j] = "Х";
+                        }
+                    }
+                    if (map[i, j] != "Х") map[i, j] = "O";
+                }
+            }
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == "Х")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{map[i, j]} ");
+                        Console.ResetColor();
+                    }    
+                    else
+                        Console.Write($"{map[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
